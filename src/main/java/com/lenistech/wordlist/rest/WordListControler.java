02cb219d1c8b1334ee3677934b42aac6e69941d8
Jenelle Kaldon <jenellekaldon@gmail.com>
2018-12-20 20:41:03 -0600

    Configured WordListController

---
+++ src/main/java/com/lenistech/wordlist/rest/WordListControler.java
@@ -0,0 +1,28 @@
+package com.lenistech.wordlist.rest;
+
+import com.google.common.collect.Lists;
+import com.lenistech.wordlist.service.WordListService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class WordListControler {
+    private WordListService wordListService = new WordListService();
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/words")
+    @ResponseBody
+    public List<String> getWordList(@RequestParam() int numWords){
+        List<String> list = new ArrayList<String>(numWords);
+        for(int i=0; i<numWords; i++){
+            list.add("word"+(i+1));
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/lenistech/wordlist/service/WordListService.java
@@ -0,0 +1,8 @@
+package com.lenistech.wordlist.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordListService {
+
+}
